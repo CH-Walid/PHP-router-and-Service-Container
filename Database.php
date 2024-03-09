87d08fa38cc2e7d586f17eb7e505f5cd2762dfe9
CHABANI Walid <chabaniw20@gmail.com>
2024-03-09 19:18:23 +0100

    setting the config file

--- Database.php
+++ Database.php
@@ -3,9 +3,10 @@
 class Database {
   protected $connection;
 
-  public function __construct() {
-    $dsn = "mysql:host=localhost;dbname=myapp;charset=utf8mb4";
-    $this->connection = new PDO($dsn, 'root');
+  public function __construct($config, $username = 'root', $password = '') {
+    $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
+
+    $this->connection = new PDO($dsn, $username, $password);
     $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   }
 
--- index.php
+++ index.php
@@ -4,8 +4,9 @@ require 'functions.php';
 require 'router.php';
 require 'Database.php';
 
+$config = require 'config.php';
 
-$pdo = new Database();
+$pdo = new Database($config['database']);
 
 $posts = $pdo->query('SELECT * FROM posts')->fetchAll();
 
