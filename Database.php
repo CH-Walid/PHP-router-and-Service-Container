8fbf7605367d679ce2e42d419e775db6aaef5195
CHABANI Walid <chabaniw20@gmail.com>
2024-03-09 19:49:11 +0100

    solve sql injection

--- Database.php
+++ Database.php
@@ -10,9 +10,9 @@ class Database {
     $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   }
 
-  public function query($query) {
+  public function query($query, $params) {
     $stmt = $this->connection->prepare($query);
-    $stmt->execute();
+    $stmt->execute($params);
     return $stmt;
   }
 }
--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+/* require 'router.php'; */
 require 'Database.php';
+
+$id_user = $_GET['id'];
 
 $config = require 'config.php';
 
 $pdo = new Database($config['database']);
 
-$posts = $pdo->query('SELECT * FROM posts')->fetchAll();
+$posts = $pdo->query("SELECT * FROM posts WHERE id = :id", ['id' => $id_user])->fetch();
 
 dd($posts);
