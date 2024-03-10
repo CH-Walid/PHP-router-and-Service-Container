79faee63f59c103e0ac8bcf751a5700e1a0a9a1b
CHABANI Walid <chabaniw20@gmail.com>
2024-03-10 01:05:48 +0100

    create note form

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
 <?php
 
-$URI = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-  '/' => 'controllers/index.php',
-  '/notes' => 'controllers/notes.php',
-  '/note' => 'controllers/note.php',
-  '/about' => 'controllers/about.php',
-  '/contact' => 'controllers/contact.php',
-];
+$routes = require 'routes.php';
 
 function abort($code = 404) {
   http_response_code($code);
@@ -23,5 +15,7 @@ function routeToController($URI, $routes) {
     abort();
   }
 }
+
+$URI = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($URI, $routes);
--- views/notes.view.php
+++ views/notes.view.php
@@ -9,7 +9,7 @@ require 'partials/banner.php';
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
     <?php if(count($notes) > 0): ?>
-      <ul class="mt-4 list-disc">
+      <ul class="my-4 mb-7 list-disc">
         <?php foreach($notes as $note): ?>
           <li>
             <a href="/note?id=<?= $note['id'] ?>" class="text-blue-700 hover:underline">
@@ -21,6 +21,8 @@ require 'partials/banner.php';
     <?php else: ?>
       <p>No notes!</p>
     <?php endif; ?>
+
+    <a href="/notes/create" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create new</a>
   </div>
 </main>
 
